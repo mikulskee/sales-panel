79f4d5a7f5d5e2bd5379ed282c8cdd4bf10e0ea2
Mateusz <administrator@MacBook-Air-ADMINISTRATOR.local>
2020-08-06 00:53:14 +0200

    Updates

---
+++ precache-manifest.37def832975370f9467d5fb5d0766986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6aee2324b2a2faa7133b105ed59082",
+    "url": "/sales-panel/index.html"
+  },
+  {
+    "revision": "b1f87bf526bb260afcff",
+    "url": "/sales-panel/static/css/main.f95a6a48.chunk.css"
+  },
+  {
+    "revision": "712774b08987240b2f9c",
+    "url": "/sales-panel/static/js/2.ef4ca5bf.chunk.js"
+  },
+  {
+    "revision": "16e72e8a7b17b4817847b2ca58cd85b3",
+    "url": "/sales-panel/static/js/2.ef4ca5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f87bf526bb260afcff",
+    "url": "/sales-panel/static/js/main.34d80a71.chunk.js"
+  },
+  {
+    "revision": "13dbaa3df9b7c2a572b4",
+    "url": "/sales-panel/static/js/runtime-main.612ebae2.js"
+  }
+]);
